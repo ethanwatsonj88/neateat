729a482b0eeb368917a93a7ee9e89443236490d9
Karmen <lu.kar@husky.neu.edu>
2020-04-24 17:45:08 -0400

    Update gatsby config file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,23 +31,24 @@ module.exports = {
     // The name of the plugin
     resolve: 'gatsby-source-mongodb',
     options: {
+        // Name of the database and collection where are books reside
         dbName: 'gatsby',
         collection: 'users',
-        // server: {
-        //  address: 'main-shard-00-01-zxsxp.mongodb.net',
-        //    port: 27017
-        //},
+        server: {
+            address: 'cluster0-shard-00-01-mhtd2.mongodb.net',
+            port: 27017
+        },
         auth: {
             user: 'neateat',
             password: 'yumYelpYum'
         },
-        // extraParams: {
-        //  replicaSet: 'Main-shard-0',
-        //  ssl: true,
-        //  authSource: 'admin',
-        //  retryWrites: true
-        // }
+        extraParams: {
+            replicaSet: 'Cluster0',
+            ssl: true,
+            authSource: 'admin',
+            retryWrites: true
+        }
       }
-    },
+    },      
   ],
 }
