f1071c4f49d851245093210eec7693272c3e4e53
ethanwatsonj88 <ethanwatsonj88@gmail.com>
2020-04-26 14:35:23 -0400

    dynamically create user pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,30 @@
 //for api
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const { data } = await graphql(`
+    {
+      users: allMongodbGatsbyUsers {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  const pageTemplate = path.resolve('./src/components/user.js')
+
+  for (const { node } of data.users.edges) {
+    createPage({
+      path: `/user/${node.id}/`,
+      component: pageTemplate,
+      context: {
+        id: node.id,
+      },
+    })
+  }
+}
